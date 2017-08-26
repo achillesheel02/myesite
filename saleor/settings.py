24cf3966ad9af01458025ec14d13048d51480334
achillesheel02 <bachillah@gmail.com>
2017-08-26 10:46:20 +0300

    static fix

--- saleor/settings.py
+++ saleor/settings.py
@@ -8,7 +8,7 @@ import dj_email_url
 from django.contrib.messages import constants as messages
 import django_cache_url
 
-DEBUG = 'True'
+DEBUG = ''
 
 SITE_ID = 1
 
@@ -70,12 +70,13 @@ ORDER_FROM_EMAIL = os.getenv('ORDER_FROM_EMAIL', DEFAULT_FROM_EMAIL)
 MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'saleor','staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     ('assets', os.path.join(PROJECT_ROOT, 'saleor', 'static', 'assets')),
     ('images', os.path.join(PROJECT_ROOT, 'saleor', 'static', 'images')),
-    ('dashboard', os.path.join(PROJECT_ROOT, 'saleor', 'static', 'dashboard'))
+    ('dashboard', os.path.join(PROJECT_ROOT, 'saleor', 'static', 'dashboard')),
+os.path.join(PROJECT_ROOT, 'static'),
 ]
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
